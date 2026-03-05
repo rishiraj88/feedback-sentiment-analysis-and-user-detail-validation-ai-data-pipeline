aeb5e4448ea6fb5bcd233675a394fb6044b99829
Rishi Raj <rishiraj88+github@gmail.com>
2026-03-05 04:44:21 +0100

    Update .gitignore and add user-batch app.

---
+++ platform/batching/user-batch/src/main/java/rr/userdetails/userbatch/UserBatchApplication.java
@@ -0,0 +1,13 @@
+package rr.userdetails.userbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBatchApplication.class, args);
+	}
+
+}
---
+++ platform/batching/user-batch/src/test/java/rr/userdetails/userbatch/UserBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package rr.userdetails.userbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
